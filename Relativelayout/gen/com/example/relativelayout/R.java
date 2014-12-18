a2557d23748a2c1324773d0ffdcfae01ade3027f
jokercard1986 <jokercard1986@gmail.com>
2014-12-18 21:58:15 +0800

    Listview

--- Relativelayout/gen/com/example/relativelayout/R.java
+++ Relativelayout/gen/com/example/relativelayout/R.java
@@ -35,7 +35,7 @@ public final class R {
         public static final int button3=0x7f080004;
         public static final int checkBox1=0x7f080002;
         public static final int editText1=0x7f080000;
-        public static final int textView1=0x7f080005;
+        public static final int listView1=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
